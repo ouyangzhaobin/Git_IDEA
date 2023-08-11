58a5b01dce4c370cdfacdddc51b799faae0a528a
ouyang <ouyangzhaobin6@163.com>
2023-08-11 20:25:21 +0800

    first commit Git_IDEA branch1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello branch1_1!");
     }
 }
